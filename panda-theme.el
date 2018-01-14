a651e8854f30a5f8d1ada7b13798f7f5aeb5ac94
Jamie Collinson <jamiecollinson@gmail.com>
2018-01-14 20:22:55 +0000

    Fix magic autoload

--- panda-theme.el
+++ panda-theme.el
@@ -105,7 +105,7 @@
    `(ansi-color-names-vector
      [,bg ,red ,purple ,orange ,blue ,pink ,cyan ,fg])))
 
-;;; autoload
+;;;###autoload
 (when load-file-name
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))))
